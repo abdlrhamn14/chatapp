6aabaa8ee43c22a15d2989a1ffd03a36b47ed6e4
mohamedibrahemanwar <mohamedibrahemofficail@gmail.com>
2023-09-22 17:13:24 +0300

    toast

--- app/src/main/java/com/route/chatapplication1/home/Route.kt
+++ app/src/main/java/com/route/chatapplication1/home/Route.kt
@@ -2,6 +2,7 @@ package com.route.chatapplication1.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.route.chatapplication1.R
 
 class Route : AppCompatActivity() {
@@ -9,4 +10,7 @@ class Route : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_route)
     }//this is comment
+    fun method(){
+        Toast.makeText(this,"hi",Toast.LENGTH_SHORT).show()
+    }
 }
