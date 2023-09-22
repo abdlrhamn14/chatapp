06e7bd03f97c8d2132adf9c3999e44c8bbd12d91
abdlrhman <abdoljkhj@gmail.com>
2023-09-22 17:03:44 +0300

    +chatApp

---
+++ app/src/main/java/com/route/chatapplication1/home/Route.kt
@@ -0,0 +1,12 @@
+package com.route.chatapplication1.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.route.chatapplication1.R
+
+class Route : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_route)
+    }
+}
